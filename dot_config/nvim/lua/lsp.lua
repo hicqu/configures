c471aab04ce19edae3010704581ef120345c7183
qupeng <qupeng@pingcap.com>
2023-05-31 16:44:58 +0800

    some fixes
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -3,9 +3,29 @@ require("mason-lspconfig").setup({
 	automatic_installation = false,
 })
 
-require("lspconfig")["jedi_language_server"].setup({})
-require("lspconfig")["lua_ls"].setup({})
-require("lspconfig")["gopls"].setup({})
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("lspconfig")["jedi_language_server"].setup({ capabilities = capabilities })
+
+require("lspconfig")["lua_ls"].setup({
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			format = {
+				enable = true,
+				-- FIXME: doesn't work.
+				defaultConfig = {
+					indent_style = "space",
+					indent_size = "4",
+				},
+			},
+		},
+	}
+})
+
+require("lspconfig")["gopls"].setup({ capabilities = capabilities })
+
+require("lspconfig")["rust_analyzer"].setup({ capabilities = capabilities })
 
 vim.diagnostic.config({
 	virtual_text = false, signs = false, underline = false,
@@ -19,23 +39,27 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 		vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, opts)
 		vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format({ async = true }) end, opts)
-
 		print("Lsp attached")
 	end,
 })
 
 local cmp = require("cmp")
+local cmp_ctx = require("cmp.config.context")
 local compare = require("cmp.config.compare")
 local types_cmp = require("cmp.types.cmp")
 local types_lsp = require("cmp.types.lsp")
 cmp.setup({
 	enabled = function()
-		local context = require "cmp.config.context"
-		if vim.api.nvim_get_mode().mode == "c" then
-			return true
-		else
-			return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+		local disabled = false
+		disabled = disabled or (vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt')
+		disabled = disabled or (vim.fn.reg_recording() ~= '')
+		disabled = disabled or (vim.fn.reg_executing() ~= '')
+		if not disabled then
+			if vim.api.nvim_get_mode().mode ~= "c" and (cmp_ctx.in_treesitter_capture("comment") or cmp_ctx.in_syntax_group("Comment")) then
+				disabled = true
+			end
 		end
+		return not disabled
 	end,
 	snippet = {
 		expand = function(args)
@@ -55,6 +79,9 @@ cmp.setup({
 		disallow_partial_fuzzy_matching = true,
 		disallow_prefix_unmatching = true,
 	},
+	experimental = {
+		ghost_text = true,
+	},
 	view = {
 		entries = "custom",
 	},
@@ -71,7 +98,7 @@ cmp.setup({
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-n>"] = cmp.mapping.select_next_item(),
 		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<CR>"] = cmp.mapping.confirm({ select = false }),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
 	}),
 	sources = cmp.config.sources({
 		{
@@ -83,8 +110,3 @@ cmp.setup({
 		{ name = "vsnip" },
 	})
 })
-
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-require("lspconfig")["jedi_language_server"].setup({ capabilities = capabilities })
-require("lspconfig")["lua_ls"].setup({ capabilities = capabilities })
-require("lspconfig")["gopls"].setup({ capabilities = capabilities })
