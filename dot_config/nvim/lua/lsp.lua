1b586936390000b0f9364e1f49b187f1cf265c90
qupeng <qupeng@pingcap.com>
2023-05-31 15:15:05 +0800

    init
    Signed-off-by: qupeng <qupeng@pingcap.com>

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -1,48 +1,90 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    automatic_installation = false,
+	automatic_installation = false,
 })
 
-local coq = require ("coq")
-require("lspconfig")["jedi_language_server"].setup(coq.lsp_ensure_capabilities({}))
-require('lspconfig')["gopls"].setup(coq.lsp_ensure_capabilities({}))
+require("lspconfig")["jedi_language_server"].setup({})
+require("lspconfig")["lua_ls"].setup({})
+require("lspconfig")["gopls"].setup({})
 
 vim.diagnostic.config({
-    virtual_text = false, signs = false, underline = false,
+	virtual_text = false, signs = false, underline = false,
 })
 
-vim.g.completeopt = "menu"
-vim.g.coq_settings = {
-    ["completion.always"] = true,
-    ["completion.smart"] = false,
-    ["keymap.recommended"] = false,
-    ["keymap.manual_complete"] = "<C-n>",
-    ["keymap.pre_select"] = true,
-}
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local opts = { buffer = args.buf }
+		vim.bo[opts.buffer].omnifunc = "v:lua.vim.lsp.omnifunc"
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format({ async = true }) end, opts)
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-      local opts = { buffer = args.buf }
-      vim.bo[opts.buffer].omnifunc = "v:lua.vim.lsp.omnifunc"
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-      vim.keymap.set('n', 'gD', vim.lsp.buf.type_definition, opts)
-
-      vim.keymap.set({'i', 'n', 'o'}, '<CR>', function()
-	  if vim.fn.pumvisible() then
-	      print("not cr")
-	      if vim.fn.complete_info().selected == -1 then
-		  return  "<C-e>" 
-	      else
-		  return "<C-y>"
-	      end
-	  else
-	      print("not cr")
-	    return 	      "<CR>"
-	  end
-      end, { expr = true })
-
-      vim.cmd("COQnow --shut-up")
-      print("Lsp attached & COQ started")
-  end,
+		print("Lsp attached")
+	end,
 })
+
+local cmp = require("cmp")
+local compare = require("cmp.config.compare")
+local types_cmp = require("cmp.types.cmp")
+local types_lsp = require("cmp.types.lsp")
+cmp.setup({
+	enabled = function()
+		local context = require "cmp.config.context"
+		if vim.api.nvim_get_mode().mode == "c" then
+			return true
+		else
+			return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+		end
+	end,
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	completion = {
+		completeopt = "menu,menuone,noselect",
+	},
+	preselect = types_cmp.PreselectMode.None,
+	sorting = {
+		comparators = { compare.exact, compare.kind, compare.sort_text },
+	},
+	matching = {
+		disallow_fuzzy_matching = true,
+		disallow_fullfuzzy_matching = true,
+		disallow_partial_fuzzy_matching = true,
+		disallow_prefix_unmatching = true,
+	},
+	view = {
+		entries = "custom",
+	},
+	window = {
+		completion = {
+			scrollbar = false,
+		}
+	},
+	formatting = {
+		fields = { 'abbr', 'kind', 'menu' },
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+	}),
+	sources = cmp.config.sources({
+		{
+			name = "nvim_lsp",
+			entry_filter = function(entry, ctx)
+				return not string.find(entry:get_completion_item().label, "%.")
+			end,
+		},
+		{ name = "vsnip" },
+	})
+})
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+require("lspconfig")["jedi_language_server"].setup({ capabilities = capabilities })
+require("lspconfig")["lua_ls"].setup({ capabilities = capabilities })
+require("lspconfig")["gopls"].setup({ capabilities = capabilities })
--- dot_config/nvim/plugins.vim
+++ dot_config/nvim/plugins.vim
@@ -6,10 +6,9 @@ Plug 'williamboman/mason.nvim', { 'do': ':MasonUpdate' }
 Plug 'williamboman/mason-lspconfig.nvim'
 Plug 'neovim/nvim-lspconfig'
 
-" Plug 'ms-jpq/coq_nvim', { 'branch': 'coq' }
-" Plug 'ms-jpq/coq.artifacts', { 'branch': 'artifacts' }
-
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-buffer'
 Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-vsnip'
+Plug 'hrsh7th/vim-vsnip'
 call plug#end()
